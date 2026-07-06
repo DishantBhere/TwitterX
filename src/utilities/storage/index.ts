75f72422e9628c8433a9d2db2c9e8f2a776364c2
Dishant Bhere <bheredishant3@gmail.com>
2026-07-07 03:08:37 +0530

    UI upgrade second time & fixing some error before deploying

--- src/utilities/storage/index.ts
+++ src/utilities/storage/index.ts
@@ -3,7 +3,12 @@ import { createClient } from "@supabase/supabase-js";
 const URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY!;
 
-export const supabase = createClient(URL, KEY);
+export const supabase = createClient(URL, KEY, {
+    auth: {
+        flowType: "pkce",
+        detectSessionInUrl: false,
+    },
+});
 
 export const uploadFile = async (file: File) => {
     const { data, error } = await supabase.storage.from("media").upload(`${Date.now()}`, file);
